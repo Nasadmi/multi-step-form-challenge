93892630f6a1f7cd5d8cc0b2f5df159c60948447
Nasadmi <samueldiego828@gmail.com>
2023-11-18 01:47:38 +0100

    Add plain view

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const sections = document.querySelectorAll('main.content section')
 
 const span = document.querySelectorAll('span.circle-step-id')
 
+const option = document.querySelectorAll('div.option')
+
+const check = document.querySelector('input#time-plain')
+
 const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 const data = {
@@ -12,10 +16,53 @@ const data = {
         email: '',
         phone: ''
     },
+
+    plain: {
+        selection: '',
+        type: ''
+    }
 }
 
+option.forEach(div => {
+    div.addEventListener('click', (e) => {
+        let selectedElement = document.querySelector('div.option.selected')
+        
+        if (e.target.classList.contains('selected')) {
+            return
+        }
+
+        if (selectedElement === null) {
+            e.target.classList.add('selected')
+        } else {
+            e.target.classList.add('selected')
+            selectedElement.classList.remove('selected')
+        }
+    })
+})
+
+check.addEventListener('change', () => {
+    const prices = document.querySelectorAll('span.price')
+    const text = document.querySelectorAll('span.text-yearly')
+    if (check.checked) {
+        prices[0].innerText = '$90/yr'
+        prices[1].innerText = '$120/yr'
+        prices[2].innerText = '$150/yr'
+        text.forEach(element => {
+            element.classList.add('show')
+        })
+    } else {
+        prices[0].innerText = '$9/mo'
+        prices[1].innerText = '$12/mo'
+        prices[2].innerText = '$15/mo'
+        text.forEach(element => {
+            element.classList.remove('show')
+        })
+    }
+    
+})
+
 const changeFocusedElement = (target) => {
-    const focusedElement = document.querySelector('span.focus')
+    let focusedElement = document.querySelector('span.focus')
     if (focusedElement) {
         span.forEach(element => {
             if (element.getAttribute('data-id') === target) {
@@ -28,32 +75,71 @@ const changeFocusedElement = (target) => {
 
 class setData {
     info() {
-        const name = document.querySelector('input#name').value
-        const email = document.querySelector('input#email').value
-        const phone = document.querySelector('input#phone').value
+        const name = document.querySelector('input#name')
+        const email = document.querySelector('input#email')
+        const phone = document.querySelector('input#phone')
 
-        if (name === '' || email === '' || phone === '') {
-            console.log('error')
+        const nameError = name.parentElement.children[0].children[0]
+        const emailError = email.parentElement.children[0].children[0]
+        const phoneError = phone.parentElement.children[0].children[0]
+
+        if (name.value === '' || email.value === '' || phone.value === '') {
+
+            nameError.innerText = 'This field is required'
+            emailError.innerText = 'This field is required'
+            phoneError.innerText = 'This field is required'
+
             return {
                 error: true
             }
         }
 
-        if (!email.match(regexEmail)) {
+        nameError.innerText = ''
+        emailError.innerText = ''
+        phoneError.innerText = ''
+
+        if (!email.value.match(regexEmail)) {
+            email.parentElement.children[0].children[0].innerText = 'Invalid email'
+
             return {
                 error: true
             }
         }
 
-        if (isNaN(phone.split('+').join('').split(' ').join(''))) {
+        nameError.innerText = ''
+        emailError.innerText = ''
+        phoneError.innerText = ''
+
+        if (isNaN(phone.value.split('+').join('').split(' ').join(''))) {
+            phone.parentElement.children[0].children[0].innerText = 'Invalid phone number'
+
             return {
                 error: true
             }
         }
 
-        data.info.name = name
-        data.info.email = email
-        data.info.phone = phone
+        data.info.name = name.value
+        data.info.email = email.value
+        data.info.phone = phone.value
+
+        return {
+            error: false
+        }
+    }
+
+    plain() {
+        const selectedElement = document.querySelector('div.option.selected')
+        if (selectedElement === null) {
+            document.querySelector('article.options').parentElement.children[2].classList.add('show')
+
+            return {
+                error: true
+            }
+        }
+
+        document.querySelector('article.options').parentElement.children[2].classList.add('show')
+        data.plain.selection = selectedElement.getAttribute('id')
+        data.plain.type = check.checked ? 'yearly' : 'monthly'
 
         return {
             error: false
@@ -74,6 +160,11 @@ btn.forEach(element => {
                 case 'info':
                     const info = sets.info()
                     err = info.error
+                break;
+
+                case 'plain':
+                    const plain = sets.plain()
+                    err = plain.error
                 break;
                 
                 case null:
